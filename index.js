44b453cbd7e15f163deb55bdb64b118dc819ccb6
Jihane Asri <jihane.asri@symphony.com>
2022-02-24 09:55:48 +0100

    Test branch protection API update

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ async function run(octokit) {
         required_status_checks: {
             strict: true,
             contexts: [
-            ],
-            checks: [
+                'default'
             ]
         },
-        enforce_admins: false,
+        enforce_admins: null,
         required_pull_request_reviews: {
             dismissal_restrictions: {},
             dismiss_stale_reviews: true,
@@ -23,24 +22,27 @@ async function run(octokit) {
             required_approving_review_count: 1,
             bypass_pull_request_allowances: null
         },
-        restrictions: false
+        restrictions: {
+            users: [
+            ],
+            teams: [
+            ],
+            apps: [
+            ]
+        }
     })
 }
 
 
 try {
-    // This should be a token with access to your repository scoped in as a secret.
-    // The YML workflow will need to set myToken with the GitHub Secret Token
-    // myToken: ${{ secrets.GITHUB_TOKEN }}
-    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const myToken = core.getInput('repo-token');
-
-    const octokit = github.getOctokit(myToken);
-    // You can also pass in additional options as a second parameter to getOctokit
-    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
-
-    // run octokit
-    run(octokit).then(r => console.log(r));
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
